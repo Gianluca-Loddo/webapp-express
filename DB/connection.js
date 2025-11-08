54f591df87a2c2bf87835b98fafac8baceba1b66
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-11-08 18:00:47 +0100

    created new file connection.js into folder DB => module.export and import into server.js

---
+++ DB/connection.js
@@ -0,0 +1,23 @@
+// utilizzo di dotenv per poter usare il file .env
+require('dotenv').config();
+
+const mysql = require('mysql2');
+
+// creazione della connessione
+const connection = mysql.createConnection(
+    {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    }
+);
+
+// gestione dell'eventuale errore in fase di connessione al database 
+connection.connect((err) =>{
+    if (err) throw err;
+    console.log('Connected to My SQL Database')
+});
+
+
+module.exports=connection;
