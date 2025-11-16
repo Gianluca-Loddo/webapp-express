b27a1679e11e1861682b90598a503765dcfe40e0
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-11-16 17:03:56 +0100

    installed cors

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
+
+app.use(cors({
+    origin: "http://localhost:5173",   // chi può accedere al backend
+    credentials: true                   // se in futuro si dovessero usare cookie/token
+}));
+
 
 app.listen(port, () => { console.log(`Il server è in ascolto sulla porta http://localhost:${port}`) });
 
